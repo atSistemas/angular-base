78b2c9b20b4b7e1cbd4874a17c37667444863b70
Rafael Bernad de Castro <rbernad@atsistemas.com>
2016-08-12 00:05:52 +0200

    Remove unnecesary file

--- server/common/node-libs.ts
+++
@@ -1,8 +0,0 @@
-
-const nodeRequire = System._nodeRequire;
-
-export const express = nodeRequire('express');
-export const path = nodeRequire('path');
-export const fs = nodeRequire('fs');
-
-export default nodeRequire;
