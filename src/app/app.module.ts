0c1b50527da52f2bc4a63fec3d467ef0253a4574
Rafael Bernad de Castro <rbernad@atsistemas.com>
2016-08-19 14:26:24 +0200

    Simplifly import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { BaseApp } from '../base/components/base/';
+import { BaseApp } from '../base';
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { HTTP_PROVIDERS } from '@angular/http';
 import { provideRouter } from '@angular/router';
 import { DevToolsExtension, NgRedux } from 'ng2-redux';
 import { NgReduxRouter } from 'ng2-redux-router';
-import { Routing, AppRoutingProviders } from '../base/routes/';
+import { Routing, RoutingProviders } from '../base';
 import { Store } from '../base/store';
 
 @NgModule({
     imports:      [ BrowserModule, Routing ],
     declarations: [ BaseApp],
-    providers:    [ AppRoutingProviders, Store, NgRedux, NgReduxRouter, HTTP_PROVIDERS, DevToolsExtension],
+    providers:    [ RoutingProviders, Store, NgRedux, NgReduxRouter, HTTP_PROVIDERS, DevToolsExtension],
     bootstrap:    [ BaseApp ]
 })
 export class BaseModule { }
