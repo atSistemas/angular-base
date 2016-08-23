3fe141c2bcdd4ceabe38919bd48299353e3fdb0a
Pablo Magaz <pmagaz@atsistemas.com>
2016-08-23 14:58:56 +0200

    Reordering imports

--- src/app/containers/main/reducers/index.ts
+++ src/app/containers/main/reducers/index.ts
@@ -1,7 +1,8 @@
-import CreateReducer from '../../../../base/shared/CreateReducer';
 import { Action } from 'redux';
+
 import { Types } from '../types';
 import { MainTypes, MainModel, InitialState } from '../models';
+import CreateReducer from '../../../../base/shared/CreateReducer';
 
 const click = (state) =>
   state.update('main', (value) => "eooo");
