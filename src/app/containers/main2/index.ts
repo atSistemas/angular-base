6f25057cfcddf548cdb4d2305848be8b30a8cb9c
Rafael Bernad de Castro <rbernad@atsistemas.com>
2016-08-23 14:07:41 +0200

    mock list

--- src/app/containers/main2/index.ts
+++ src/app/containers/main2/index.ts
@@ -5,13 +5,20 @@ import { Observable } from 'rxjs/Observable';
 import { DevToolsExtension, NgRedux, select } from 'ng2-redux';
 import { NgReduxRouter } from 'ng2-redux-router';
 import { createEpicMiddleware } from 'redux-observable';
+import { CORE_DIRECTIVES } from '@angular/common';
+import { people, Person } from './mocks/people';
 
 @Component({
   selector: 'main-container2',
   templateUrl: './main.html',
+  directives: [CORE_DIRECTIVES],
   pipes: [ AsyncPipe ]
 })
 export class MainContainer2 {
   private devTools: DevToolsExtension;
   private ngReduxRouter: NgReduxRouter;
+  people: Person[];
+  constructor() {
+    this.people = people;
+  }
 }
