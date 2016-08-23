eda71291efcd56cd08fdb01241c74d734444c487
Rafael Bernad de Castro <rbernad@atsistemas.com>
2016-08-23 11:52:59 +0200

    Create directive for state reading (WIP)

---
+++ src/app/containers/main/components/reducer-display.ts
@@ -0,0 +1,15 @@
+
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { MainTypes } from '../models';
+
+@Component({
+  selector: 'main-display',
+  template: `InitialState = {{main}}`,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class MainDisplay {
+  @Input() main: MainTypes;
+  constructor() {
+    console.log(this)
+  }
+}
