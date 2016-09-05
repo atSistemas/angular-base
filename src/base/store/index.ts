d78a2c7ae5814dd3c934348068143cf5726f1564
Pablo Magaz <pmagaz@atsistemas.com>
2016-09-05 22:07:29 +0200

    Add main epic middleware

--- src/base/store/index.ts
+++ src/base/store/index.ts
@@ -3,8 +3,12 @@ import { DevToolsExtension, NgRedux, select } from 'ng2-redux';
 import { RootReducer } from '../reducers';
 import * as createLogger from 'redux-logger';
 import { MainModelInterface } from '../models';
-
+import { createEpicMiddleware } from 'redux-observable';
+import { createStore, applyMiddleware } from 'redux';
+import { MainService } from '../../app/containers/main/services/main.service';
 const middleware = [];
+
+//const epicMiddleware = createEpicMiddleware(RootEpic);
 
 export interface AppState {
   main?: MainModelInterface;
@@ -12,8 +16,9 @@ export interface AppState {
 
 @Injectable()
 export class Store {
-  constructor(private ngRedux: NgRedux<AppState>) {
-  }
+  constructor(private ngRedux: NgRedux<AppState>,
+  private epics: MainService) {}
+
   configureStore(){
 
     middleware.push(
@@ -21,6 +26,10 @@ export class Store {
         level: 'info',
         collapsed: true,
       }));
+
+    //FIXME COMBINE EPICS
+    //middleware.push(epicMiddleware);
+    middleware.push(createEpicMiddleware(this.epics.getData));
 
     this.ngRedux.configureStore(RootReducer, {}, middleware);
   }
