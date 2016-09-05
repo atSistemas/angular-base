69c6ff042d5a80d26bee2fb189e65a958ac4b34e
Pablo Magaz <pmagaz@atsistemas.com>
2016-09-05 22:07:48 +0200

    Add main request action

--- src/app/containers/main/actions/index.ts
+++ src/app/containers/main/actions/index.ts
@@ -1,8 +1,13 @@
-
+import { Action } from 'redux';
 import { Injectable } from '@angular/core';
 import { NgRedux } from 'ng2-redux';
 import { AppState } from '../../../../base/store';
 import { actionTypes } from '../action-types';
+
+//FIXME MOVE TO CORRECT PLACE
+export interface IPayloadAction extends Action {
+  payload?: any;
+}
 
 @Injectable()
 export class MainActions {
@@ -19,5 +24,9 @@ export class MainActions {
 
   click() {
     this.ngRedux.dispatch({ type: actionTypes.CLICK });
+  }
+
+  mainRequest(){
+    this.ngRedux.dispatch({ type: actionTypes.MAIN_REQUEST });
   }
 }
