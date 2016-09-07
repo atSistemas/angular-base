5d623762a6f4f83b54f0496b1a440d995204f821
Rafael Bernad de Castro <rbernad@atsistemas.com>
2016-09-07 17:54:54 +0200

    Lazy service

--- src/app/containers/+lazy/services/lazy-service.ts
+++ src/app/containers/+lazy/services/lazy-service.ts
@@ -1,17 +1,33 @@
+import 'rxjs';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/catch';
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http } from '@angular/http';
+import { ActionsObservable } from 'redux-observable';
+import { Observable } from 'rxjs/Observable';
 
-import { Observable } from 'rxjs';
+import { actionTypes } from '../action-types';
+import { IPayloadAction } from '../../main/actions'
+import { AppState } from '../../../../base/store';
+
 
 @Injectable()
 export class LazyService {
   constructor(private http: Http) {}
 
-  getMain(): Promise<any> {
-    return this.http
-      .get('mocks/people.json')
-      .toPromise()
-      .then((res) => res.json())
-      .catch((res) => res.json());
-  }
+  getData = (action$: ActionsObservable) => {
+    return action$.ofType(actionTypes.LAZY_REQUEST)
+    .flatMap(() => {
+      return this.http.get('mocks/main.json')
+        .map(result => ({
+          type: actionTypes.LAZY_SUCCESS,
+          payload: result.json()
+        }))
+        .catch(error => Observable.of({
+          type: actionTypes.LAZY_ERROR
+        }));
+      });
+    }
 }
