43936a524c44d140d3b448c651ef3995dcea7ce3
Rafael Bernad de Castro <rbernad@atsistemas.com>
2016-09-07 13:13:56 +0200

    Replace object by Enumerable in actionTypes

--- src/app/containers/main/action-types/index.ts
+++ src/app/containers/main/action-types/index.ts
@@ -1,8 +1,3 @@
-export const actionTypes = {
-  CLICK: 'CLICK',
-  MAIN_CONTAINER: 'MAIN_CONTAINER',
-  MAIN_REQUEST: 'MAIN_REQUEST',
-  MAIN_SUCCESS: 'MAIN_SUCCESS',
-  MAIN_ERROR: 'MAIN_ERROR',
-  GOTO_MAINCONTAINER2: 'GOTO_MAINCONTAINER2'
-};
+import Enumerable from '../../../../base/shared/Enumerable';
+
+export const actionTypes = new Enumerable('CLICK', 'MAIN_CONTAINER', 'MAIN_REQUEST', 'MAIN_SUCCESS', 'MAIN_ERROR', 'GOTO_MAINCONTAINER2');
