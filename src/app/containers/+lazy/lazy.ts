e37a75a0aacd44c2b2f2913f6fbe6615d16b1094
Rafael Bernad de Castro <rbernad@atsistemas.com>
2016-09-07 17:54:36 +0200

    Lazy Redux support (WIP)

--- src/app/containers/+lazy/lazy.ts
+++ src/app/containers/+lazy/lazy.ts
@@ -2,13 +2,16 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { DevToolsExtension, NgRedux, select } from 'ng2-redux';
 import { NgReduxRouter } from 'ng2-redux-router';
-
+import { AppState } from '../../../base/store';
+import { combineReducers } from 'redux';
+import { LazyReducer } from './reducers';
 import { LazyService } from './services/lazy-service';
+import { LazyActions } from './actions';
 
 @Component({
   selector: 'lazy-container',
   templateUrl: './lazy.html',
-  providers: [LazyService],
+  providers: [LazyActions, LazyService],
 })
 
 export class LazyContainer {
@@ -16,9 +19,17 @@ export class LazyContainer {
   private ngReduxRouter: NgReduxRouter;
 
   public people: Object[];
-
-  constructor() { }
+  public Reducers: any;
+  constructor(
+    private actions: LazyActions
+  ) { }
 
   ngOnInit(): void {
+    console.log("init lazy")
+    this.Reducers = combineReducers<AppState>({
+      lazy: LazyReducer
+    });
+    console.log(this.Reducers);
+    this.actions.lazyRequest();
   }
 }
