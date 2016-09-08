fe04a2121d79fccf8f89c764e871a8b91f966f25
Rafael Bernad de Castro <rbernad@atsistemas.com>
2016-09-08 16:07:27 +0200

    rename externals to dll

---
+++ webpack/dll/helpers.ts
@@ -0,0 +1,34 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { iManifest, externalsPath, assetsPath } from './index';
+
+export function getPolyfills(): string[] {
+    return require('../../package.json')._polyfills;
+}
+
+export function getVendorModules(env?: any): Array<string> {
+  return Object.keys(require('../../package.json').dependencies);
+}
+
+
+export function root(__path: string = '.'): string {
+    return path.join(__dirname, '..', '..', __path);
+}
+
+export function getManifest(__path: string): iManifest {
+    var manifest = JSON.parse(fs.readFileSync(root(externalsPath + '/' + __path + '-manifest.json'), 'utf8')
+        // TODO(gdi2290): workaround until webpack fixes dll generation
+        .replace(/}(.*[\n\r]\s*)}(.*[\n\r]\s*)}"activeExports": \[\]/, ''));
+    return manifest;
+}
+export function getExternals(chunk: string): string {
+    var assets = require(root(`${externalsPath}/webpack-assets.json`));
+    // {"vendor":{"js":"vendor.js"},"polyfills":{"js":"polyfills.js"}}
+    return assets[chunk]['js']
+}
+export function getAssets(chunk: string): string {
+    var assets = require(root(`${assetsPath}/webpack-assets.json`));
+    // {"vendor":{"js":"vendor.js"},"polyfills":{"js":"polyfills.js"}}
+    return assets[chunk]['js']
+}
+
