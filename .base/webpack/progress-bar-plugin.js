9329bb9d30960878e3ccf9bcd2d28db0440deb4c
Rafael Bernad de Castro <rbernad@atsistemas.com>
2016-09-08 17:57:56 +0200

    align line with the rest of content

--- .base/webpack/progress-bar-plugin.js
+++ .base/webpack/progress-bar-plugin.js
@@ -3,7 +3,7 @@ const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 
 module.exports = function () {
     return new ProgressBarPlugin({
-        format: `[BASE] ${console.color('info', 'i')} Bundling... [:bar] ${console.color('success', ':percent')} (:elapsed seconds)`,
+        format: `  [BASE] ${console.color('info', 'i')} Bundling... [:bar] ${console.color('success', ':percent')} (:elapsed seconds)`,
         clear: true,
         summary: false,
     });
