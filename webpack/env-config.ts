aaae24baea5ce9302f8cf84a36964d88eab46b9a
Rafael Bernad de Castro <rbernad@atsistemas.com>
2016-09-08 15:49:19 +0200

    proper env-config naming

---
+++ webpack/env-config.ts
@@ -0,0 +1,18 @@
+
+import * as development from './webpack-config-dev';
+import * as production from './webpack-config-prod';
+import environment, { constants as envConstants } from '../server/environment';
+
+export interface envConfig {
+    devtool: string,
+    entry: { [propName: string]: any },
+    context: string,
+    plugins: any[],
+    preLoaders: any[],
+    loaders: any[],
+    postCss: Function
+}
+
+const envConfig: envConfig = (environment.ENV === envConstants.DEVELOPMENT) ? development : production;
+
+export default envConfig; 
