e86aa6c7e26da616a16c0e146efb1e5b7ba3b6eb
Rafael Bernad de Castro <rbernad@atsistemas.com>
2016-09-08 10:18:21 +0200

    Initialize redux bindings in module init instead of component init

--- src/app/containers/+lazy/index.ts
+++ src/app/containers/+lazy/index.ts
@@ -3,8 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { LazyContainer } from './lazy';
 
+import { AppState } from '../../../base/store';
+import { combineReducers } from 'redux';
+import { LazyReducer } from './reducers';
+import { LazyService } from './services/lazy-service';
+import { LazyActions } from './actions';
+
 @NgModule({
   declarations: [LazyContainer],
+  providers: [LazyActions, LazyService],
   imports: [
     CommonModule,
     RouterModule.forChild([
@@ -12,9 +19,16 @@ import { LazyContainer } from './lazy';
     ])
   ]
 })
-export default class LazyContainerModule implements OnInit {
-
-  ngOnInit(): void {
-    console.log("child module init")
+export default class LazyContainerModule {
+  public Reducers: any;
+  constructor(private actions: LazyActions) { 
+    this.init();
+  }
+  init(): void {
+    this.Reducers = combineReducers<AppState>({
+      lazy: LazyReducer
+    });
+    console.log(this.Reducers);
+    this.actions.lazyRequest();
   }
 }
--- src/app/containers/+lazy/lazy.ts
+++ src/app/containers/+lazy/lazy.ts
@@ -2,16 +2,11 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { DevToolsExtension, NgRedux, select } from 'ng2-redux';
 import { NgReduxRouter } from 'ng2-redux-router';
-import { AppState } from '../../../base/store';
-import { combineReducers } from 'redux';
-import { LazyReducer } from './reducers';
-import { LazyService } from './services/lazy-service';
-import { LazyActions } from './actions';
+
 
 @Component({
   selector: 'lazy-container',
-  templateUrl: './lazy.html',
-  providers: [LazyActions, LazyService],
+  templateUrl: './lazy.html'
 })
 
 export class LazyContainer {
@@ -19,17 +14,5 @@ export class LazyContainer {
   private ngReduxRouter: NgReduxRouter;
 
   public people: Object[];
-  public Reducers: any;
-  constructor(
-    private actions: LazyActions
-  ) { }
-
-  ngOnInit(): void {
-    console.log("init lazy")
-    this.Reducers = combineReducers<AppState>({
-      lazy: LazyReducer
-    });
-    console.log(this.Reducers);
-    this.actions.lazyRequest();
-  }
+  constructor( ) { }
 }
