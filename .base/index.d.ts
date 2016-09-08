e5cf0cfb1cdf00a6b7b3a6f442735f48c506f1ef
Rafael Bernad de Castro <rbernad@atsistemas.com>
2016-09-08 16:56:45 +0200

    Add webpack namespace to base

--- .base/index.d.ts
+++ .base/index.d.ts
@@ -1,11 +1,15 @@
 declare module base {
-    export module console {
-      export function info(...messages: any[]): any;
-      export function success(...messages: any[]): any;
-      export function error(...messages: any[]): any;
-      export function line(...messages: any[]): any;
-      export function clear(...messages: any[]): void;
-    }
+  export module console {
+    export function info(...messages: any[]): any;
+    export function success(...messages: any[]): any;
+    export function error(...messages: any[]): any;
+    export function line(...messages: any[]): any;
+    export function clear(...messages: any[]): void;
+  }
+  export module webpack {
+    export function ProgressBarPlugin(): void;
+    export function CompileErrorsPlugin(): void;
+  }
 }
 
 
--- .base/index.js
+++ .base/index.js
@@ -1,7 +1,13 @@
 const console = require('./console');
 const timer = require('./timer');
+const ProgressBar = require('./webpack/progress-bar-plugin');
+const CompileErrors = require('./webpack/compile-errors-plugin');
 
 module.exports = {
     console: console,
-    timer: timer
+    timer: timer,
+    webpack: {
+        ProgressBarPlugin: ProgressBar,
+        CompileErrorsPlugin: CompileErrors
+    }
 };
