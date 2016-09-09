0a9504341777af2edcb9a832ecca808d11905f23
Rafael Bernad de Castro <rbernad@atsistemas.com>
2016-09-09 10:31:19 +0200

    AsyncRedux decorator

--- src/base/decorators/async-redux.ts
+++ src/base/decorators/async-redux.ts
@@ -1,38 +1,80 @@
-import { Action } from 'redux';
+import { Action, combineReducers } from 'redux';
 //  FIXME: This won't work; we need a callable method to get the reducers properly'
 import { BaseReducers } from '../../base';
+import { AppState } from '../../base/store';
 
 export interface AsyncReduxOptions {
     reducers: { [index: string]: Action },
-    epics: { [index: string]: string[] }
+    epics: { [index: string]: string[] },
+    redux?: string
+    store?: string
 }
 
 function AsyncRedux(options: AsyncReduxOptions) {
     return function (target: Function) {
 
+        let constructor = function () {
+            target.apply(this);
+            this.asyncReduxInit();
+        };
+
+        constructor.prototype = Object.create(target.prototype);
+        constructor.prototype.constructor = target;
+
+        constructor.prototype.epics = [];
+        constructor.prototype.asyncReduxInit = () => {
+            if(!this.asyncReduxChecks()) return;
+            this.asyncReduxMapEpics();
+            this.asyncReduxMepReducers();
+            this.asyncReduxReconfigure();
+        }
+
+        constructor.prototype.asyncReduxChecks = () => {
+            if (!this.ngRedux && !this[options.redux]) {
+                console.error("You must inject ngRedux to use AsyncRedux decorator");
+                return false;
+            }
+            if (!this.store && !this[options.store]) {
+                console.error("You must inject a Redux store to use AsyncRedux decorator");
+                return false;
+            }
+            for (var key in options.epics) {
+                if (!this[key]) {
+                    console.error(`You must inject a ${key} service to declare an epic for it`);
+                    return false;
+                } else {
+                    for (var i = 0; i < options.epics[key].length; i++) {
+                        if (!this[key][options.epics[key][i]]) {
+                            console.error(`${key} doesn't have a method ${options.epics[key][i]}`);
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+
+        constructor.prototype.asyncReduxMapEpics = () => {
+            for(var key in options.epics) {
+                this.asyncReduxMapEpicMethods(this[key], options.epics[key])
+            }
+            
+        }
+
+        constructor.prototype.asyncReduxMapEpicMethods = (service:any, methods: string[]) => {
+            methods.forEach((method) => this.epics.push(service[method]));
+        }
+
+        constructor.prototype.asyncReduxMapReducers = () => {
+            this.reducers = options.reducers;
+        }
+
+        constructor.prototype.asyncReduxReconfigure = () => {
+            this.ngRedux.replaceReducer(combineReducers<AppState>(Object.assign(BaseReducers, this.reducers)));
+            this.epics.forEach((epic) => this.store.epic$.nexts(epic));
+        }
+        return <any>constructor;
+            
     }
 }
 
 export default AsyncRedux;
-
-/*
-export default class LazyContainerModule {
-  public reducers: any;
-  public epics: Observable<Action>[] = [];
-  constructor(private ngRedux: NgRedux<AppState>, private store: Store, private actions: LazyActions, lazyService: LazyService) {
-    this.epics.push(lazyService.getData);
-    this.init();
-  }
-  init(): void {
-    this.reducers = {
-      lazy: LazyReducer
-    };
-    this.replaceReducers()
-    this.actions.lazyRequest();
-  }
-  replaceReducers() {
-    this.ngRedux.replaceReducer(combineReducers<AppState>(Object.assign(BaseReducers, this.reducers)));
-    this.epics.forEach((epic) => this.store.epic$.next(epic));
-  }
-}
-*/
