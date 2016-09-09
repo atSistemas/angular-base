2bdee47efb6383e073b2719be594ad7a9eea6303
Pablo Magaz <pmagaz@atsistemas.com>
2016-09-09 13:32:23 +0200

    Clean webpack dev config

--- webpack/webpack-config-dev.ts
+++ webpack/webpack-config-dev.ts
@@ -1,14 +1,18 @@
 ///<reference path="../node_modules/@types/node/index.d.ts"/>
+
 import * as base from '../.base';
 import environment from '../server/environment';
 import * as common from './webpack-config-common';
 import { getPolyfills, getManifest, root } from './dll';
+
 const { ForkCheckerPlugin, TsConfigPathsPlugin} = require('awesome-typescript-loader');
 const { HotModuleReplacementPlugin } = require('webpack');
 const AssetsPlugin = require('assets-webpack-plugin');
 
 export const devtool = 'cheap-source-map';
-
+export const preLoaders = common.preLoaders;
+export const loaders = common.loaders;
+export const postCss = common.postCss;
 export const context = common.context;
 
 export const entry = {
@@ -21,9 +25,3 @@ export const entry = {
 export const plugins = [
   new HotModuleReplacementPlugin()
 ].concat(common.plugins);
-
-export const preLoaders = common.preLoaders;
-
-export const loaders = common.loaders;
-
-export const postCss = common.postCss;
