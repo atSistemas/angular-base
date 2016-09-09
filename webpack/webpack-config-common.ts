bed603c4d206c5a19e68f3f7a33b2bb20dba4d00
Pablo Magaz <pmagaz@atsistemas.com>
2016-09-09 13:31:44 +0200

    Clean common config

--- webpack/webpack-config-common.ts
+++ webpack/webpack-config-common.ts
@@ -1,16 +1,16 @@
 ///<reference path="../node_modules/@types/node/index.d.ts"/>
+
 import * as base from '../.base';
 import environment from '../server/environment';
 import { getPolyfills, getManifest, root } from './dll';
+
 const ProgressBarPlugin = require('progress-bar-webpack-plugin');
 const { ForkCheckerPlugin, TsConfigPathsPlugin} = require('awesome-typescript-loader');
 const { ContextReplacementPlugin, HotModuleReplacementPlugin, DefinePlugin, DllReferencePlugin, } = require('webpack');
 const AssetsPlugin = require('assets-webpack-plugin');
 
 export const devtool = 'source-map';
-
 export const context = __dirname;
-
 export const entry = {
     application: getPolyfills().concat(
         './src/app'
@@ -36,21 +36,22 @@ export const plugins = [
     //  TODO: Enable ForkCheckerPlugin as soon as the plugin is fixed
     //new ForkCheckerPlugin(),
     new base.webpack.CompileErrorsPlugin()
-    
+
 ];
 
 export const preLoaders = [
-    {
-        test: /\.ts$/,
-        loader: 'string-replace-loader',
-        query: {
-            search: '(System|SystemJS)(.*[\\n\\r]\\s*\\.|\\.)import\\((.+)\\)',
-            replace: '$1.import($3).then(mod => mod.__esModule ? mod.default : mod)',
-            flags: 'g'
-        },
-        include: [root('src')]
+  {
+    test: /\.ts$/,
+    loader: 'string-replace-loader',
+    query: {
+        search: '(System|SystemJS)(.*[\\n\\r]\\s*\\.|\\.)import\\((.+)\\)',
+        replace: '$1.import($3).then(mod => mod.__esModule ? mod.default : mod)',
+        flags: 'g'
     },
+    include: [root('src')]
+  },
 ];
+
 export const loaders = [
     {
         test: /\.ts$/,
