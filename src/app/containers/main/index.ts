45eef98173198d52a17cc1654da9dfc60e198325
Rafael Bernad de Castro <rbernad@atsistemas.com>
2016-09-12 18:26:05 +0200

    Leave binding as it was before

--- src/app/containers/main/index.ts
+++ src/app/containers/main/index.ts
@@ -14,7 +14,7 @@ export { MainDisplay } from './components';
 })
 
 export class MainContainer implements OnInit{
-  @select(state=>state.main.main) main:Observable<any>
+  @select() main$:Observable<any>
   //@select(state=>state.main) main:Observable<any>
   //@select(['main','main']) main$;
 
