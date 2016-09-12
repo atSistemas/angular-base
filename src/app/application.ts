1942d64c0928cee38a139c8dfead772ab408f2fd
Rafael Bernad de Castro <rbernad@atsistemas.com>
2016-09-12 18:22:56 +0200

    Remove unnecesary imports

--- src/app/application.ts
+++ src/app/application.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { BaseApp } from '../base';
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { HttpModule } from '@angular/http';
 import { DevToolsExtension, NgRedux } from 'ng2-redux';
 import { NgReduxRouter } from 'ng2-redux-router';
@@ -12,15 +11,13 @@ import { Store } from '../base/store';
 import { MainContainer, MainDisplay } from './containers'
 import { MainService } from './containers/main/services/main-service';
 import { MainReducer } from './containers/main/reducers';
-import { MainActions } from './containers/main/actions';
 import { BaseReduxify } from '../base/decorators';
-import { AppState } from '../base';
 
 
 @NgModule({
   imports: [BrowserModule, HttpModule, Routing],
   declarations: [BaseApp, MainContainer, MainDisplay],
-  providers: [RoutingProviders, Store, NgRedux, NgReduxRouter, DevToolsExtension, MainService, MainActions],
+  providers: [RoutingProviders, Store, NgRedux, NgReduxRouter, DevToolsExtension, MainService],
   bootstrap: [BaseApp]
 })
 @BaseReduxify({
