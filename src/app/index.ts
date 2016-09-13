ab8a729d263635009b347a485ee18b8ee844d81a
Rafael Bernad de Castro <rbernad@atsistemas.com>
2016-09-13 18:09:24 +0200

    Remove unnecesary polyfills

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,8 +1,4 @@
 import 'reflect-metadata';
-import 'babel-polyfill';
-import 'core-js/es6';
-import 'core-js/es7/reflect';
-import 'zone.js/dist/zone';
 import 'ts-helpers';
 
 import {platformBrowser} from '@angular/platform-browser';
