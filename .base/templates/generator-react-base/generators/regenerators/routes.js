59de0c28c58cc3e3086eb6bff100816f61794246
Rafael Bernad de Castro <rbernad@atsistemas.com>
2016-09-21 12:14:20 +0200

    Add log information

--- .base/templates/generator-react-base/generators/regenerators/routes.js
+++ .base/templates/generator-react-base/generators/regenerators/routes.js
@@ -2,18 +2,21 @@ import * as base from '../../../../../.base'
 import * as path from 'path';
 import _s from 'underscore.string';
 
-console.log(base)
 const template = path.join(__dirname, 'templates', '_routes.ts');
 //const destination = 
 export default function regenerateRoutes() {
 
   const folders = base.fs.getDirectories(path.join(this.basePath, 'containers'));
 
-  const routes = folders.map((folder) => ({
-    lazy: _s.startsWith(folder, '+'),
-    home: this.home === folder,
-    path: folder.replace(/^\+/, '')
-  }));
+  const routes = folders.map((folder) => {
+
+    base.console.success(`'${folder}' container linked successfully`)
+    return {
+      lazy: _s.startsWith(folder, '+'),
+      home: this.home === folder,
+      path: folder.replace(/^\+/, '')
+    }
+  });
 
   this.fs.copyTpl(
     template,
