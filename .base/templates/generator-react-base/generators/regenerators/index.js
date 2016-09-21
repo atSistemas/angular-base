b2cf2678c7ec43125c5f15c4d0a1ffd3c3815268
Rafael Bernad de Castro <rbernad@atsistemas.com>
2016-09-21 12:13:57 +0200

    force overwrite for regenerate method

--- .base/templates/generator-react-base/generators/regenerators/index.js
+++ .base/templates/generator-react-base/generators/regenerators/index.js
@@ -3,6 +3,7 @@ import * as path from 'path';
 import regenerateRoutes from './routes';
 
 export default function regenerate() {
-    base.console.info('Rebuilding application routes...');
+    base.console.info('Linking application routes...');
+    this.conflicter.force = true;
     regenerateRoutes.call(this);
 }
