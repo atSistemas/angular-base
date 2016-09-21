b7a362d493c94e9b6a99535630efb49f8c2d6dd6
Rafael Bernad de Castro <rbernad@atsistemas.com>
2016-09-21 16:38:13 +0200

    fix import

--- .base/templates/generator-react-base/generators/regenerators/index.js
+++ .base/templates/generator-react-base/generators/regenerators/index.js
@@ -1,6 +1,6 @@
 import * as base from '../../../../../.base'
 import * as path from 'path';
-import regenerateRoutes from './routes';
+import linkRoutes from './routes';
 import linkComponents from './components';
 
 export default function regenerate() {
