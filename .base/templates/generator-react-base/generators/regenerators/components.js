28faad022d3d4b19c9a57f6ca5acf01dda60ad65
Rafael Bernad de Castro <rbernad@atsistemas.com>
2016-09-21 16:39:13 +0200

    Add support for shared components

--- .base/templates/generator-react-base/generators/regenerators/components.js
+++ .base/templates/generator-react-base/generators/regenerators/components.js
@@ -6,11 +6,11 @@ const template = path.join(__dirname, 'templates', '_components.ts');
 
 function processContainer(container) {
 
-  const containerPath = path.join(this.basePath, 'containers', container);
+  const containerPath = container ? path.join(this.basePath, 'containers', container) : this.basePath;
   const componentsPath = this.destinationPath(path.join(containerPath, 'components'))
   
   if (base.fs.fileExists(componentsPath)) {
-    base.console.info(`Linking '${container}' components...`)
+    base.console.info(`Linking ${container || 'shared'} components...`)
     let components = base.fs.readDir(componentsPath);
     components = components.filter((component) => !(/^index\.?[a-z]{2,}$/.test(component)));
     components.forEach((component) => base.console.success(`'${component}'`));
@@ -34,5 +34,8 @@ export default function linkComponents() {
   const containers = base.fs.getDirectories(containersPath);
 
   containers.forEach((container) => processContainer.call(this, container));
+
+  //Process shared components
+  processContainer.call(this);
 
 }
