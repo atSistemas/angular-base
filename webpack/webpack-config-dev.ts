e41d21fa23e4791f67598046fe7e5961e52367cf
Pablo Magaz <pmagaz@atsistemas.com>
2016-10-03 13:53:38 +0200

    More clean

--- webpack/webpack-config-dev.ts
+++ webpack/webpack-config-dev.ts
@@ -1,12 +1,11 @@
 import * as base from '../.base';
-import environment, { isTesting } from '../server/environment';
 import * as common from './webpack-config-common';
-import { getPolyfills, getManifest, root } from './dll';
+import environment, { isTesting } from '../server/environment';
 
-const { HotModuleReplacementPlugin } = require('webpack');
+const webpack = require('webpack');
 const AssetsPlugin = require('assets-webpack-plugin');
 
-export const devtool = 'eval-source-map';
+export const devtool = 'cheap-source-map';
 
 export const entry = {
   application: common.entry.application.concat(
@@ -14,21 +13,26 @@ export const entry = {
     'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=false'
   )
 };
-/*
-export const entry = {
-  application: [
-    common.entry.application,
-    'webpack/hot/dev-server',
-    'webpack-hot-middleware/client'
-  ],
-}*/
 
 export const context = common.context;
 export const plugins = [
-  new HotModuleReplacementPlugin()
+  new webpack.DefinePlugin({'process.env': {'NODE_ENV': '"development"'}}),
+  new webpack.HotModuleReplacementPlugin(),
+  new AssetsPlugin({
+      path: common.buildPath,
+      filename: 'webpack-assets.json',
+      prettyPrint: true
+  }),
+  new webpack.DllReferencePlugin({
+    context: context,
+    manifest: require(`${common.manifestPath}/vendor-manifest.json`)
+  }),
+  new webpack.DllReferencePlugin({
+    context: context,
+    manifest: require(`${common.manifestPath}/polyfills-manifest.json`)
+  }),
 ].concat(common.plugins);
 
 export const module = common.module;
-//export const postCss = common.postCss;
 export const output = common.output;
 export const resolve = common.resolve;
