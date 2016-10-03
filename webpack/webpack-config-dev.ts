0813a3b2d1933bee3ce11707dcbdad13f46aa569
Pablo Magaz <pmagaz@atsistemas.com>
2016-10-03 12:03:31 +0200

    Simplify dev config

--- webpack/webpack-config-dev.ts
+++ webpack/webpack-config-dev.ts
@@ -3,15 +3,10 @@ import environment, { isTesting } from '../server/environment';
 import * as common from './webpack-config-common';
 import { getPolyfills, getManifest, root } from './dll';
 
-const { ForkCheckerPlugin, TsConfigPathsPlugin} = require('awesome-typescript-loader');
 const { HotModuleReplacementPlugin } = require('webpack');
 const AssetsPlugin = require('assets-webpack-plugin');
 
 export const devtool = 'eval-source-map';
-export const debug = true;
-export const rules = common.rules;
-export const postCss = common.postCss;
-export const context = common.context;
 
 export const entry = {
   application: common.entry.application.concat(
@@ -19,7 +14,21 @@ export const entry = {
     'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=20000&reload=false'
   )
 };
+/*
+export const entry = {
+  application: [
+    common.entry.application,
+    'webpack/hot/dev-server',
+    'webpack-hot-middleware/client'
+  ],
+}*/
 
+export const context = common.context;
 export const plugins = [
   new HotModuleReplacementPlugin()
 ].concat(common.plugins);
+
+export const module = common.module;
+//export const postCss = common.postCss;
+export const output = common.output;
+export const resolve = common.resolve;
