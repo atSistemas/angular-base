9702f60c6fb0f7baa8525d93aa38231eb1e72d2b
Pablo Magaz <pmagaz@atsistemas.com>
2016-10-03 12:03:49 +0200

    Clean code

--- webpack/index.ts
+++ webpack/index.ts
@@ -1,46 +1,5 @@
-import * as path from 'path';
-import envConfig, { isTesting } from './env-config';
-import * as base from '../.base';
-import { root } from './dll';
+import env from '../src/base/shared/Env';
+import * as development from './webpack-config-dev';
+import * as production from './webpack-config-prod';
 
-const webpackConfig = {
-
-  devtool: envConfig.devtool,
-
-  entry: envConfig.entry,
-
-  context: path.resolve(envConfig['context'], '../'),
-
-  plugins: envConfig.plugins,
-
-  //postcss: envConfig.postCss,
-
-  /*tslint: {
-    emitErrors: false,
-    failOnHint: false,
-    formattersDirectory: './node_modules/custom-tslint-formatters/formatters',
-    formatter: 'grouped'
-  },*/
-
-  module: {
-    rules: envConfig.rules
-  },
-
-  output: isTesting ? {} : {
-    path: root('build'),
-    filename: '[name].bundle.js',
-    sourceMapFilename: '[name].map',
-    chunkFilename: '[id].chunk.js',
-    publicPath: '/'
-  },
-
-  resolve: {
-    extensions: ['.js', '.ts', '.tsx', '.css'],
-    alias: {
-      'base': path.resolve(__dirname, '../src/base')
-    }
-  }
-
-};
-
-export default webpackConfig;
+export default (env == 'development') ? development : production;
