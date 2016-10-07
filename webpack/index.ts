6e55565c2d8f9ffee40b93d0999c77f48f73a5e0
Pablo Magaz <pmagaz@atsistemas.com>
2016-10-07 16:57:55 +0200

    Change env export

--- webpack/index.ts
+++ webpack/index.ts
@@ -1,5 +1,5 @@
-import env from '../src/base/shared/Env';
+import { ENV } from '../src/base/shared/Env';
 import * as development from './webpack.dev.config';
 import * as production from './webpack.prod.config';
 
-export default (env == 'development') ? development : production;
+export default (ENV == 'development') ? development : production;
