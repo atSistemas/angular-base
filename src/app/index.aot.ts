bf8d3940ea7ac764b6792a75eb6c92b6f0b18fdc
Pablo Magaz <pmagaz@atsistemas.com>
2017-01-03 13:16:37 +0100

    Update aot index file

--- src/app/index.aot.ts
+++ src/app/index.aot.ts
@@ -1,11 +1,9 @@
-import '../base/polyfills';
-
 import { enableProdMode } from '@angular/core';
 import { platformBrowser } from '@angular/platform-browser';
 import { AppModuleNgFactory } from '../compiled/src/app/app.module.ngfactory';
 
 enableProdMode();
-
+console.log('AOT MODE');
 platformBrowser()
   .bootstrapModuleFactory(AppModuleNgFactory)
   .catch(err => console.error(err));
