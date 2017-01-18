093873e103b5d3e90d0e40b9d22cc19505b2ec02
Pablo Magaz <pmagaz@atsistemas.com>
2017-01-18 10:21:58 +0100

    Remove ?

--- server/index.js
+++ server/index.js
@@ -3,8 +3,7 @@ const base = require('../.base');
 const Server = require('./server').default;
 const baseEnv = require('../src/base/shared/Env');
 
-const EnvName = (baseEnv.ENV == 'development') ?
-              ? 'Development': 'Production';
+const EnvName = (baseEnv.ENV == 'development') ? 'Development': 'Production';
 base.console.info(`Starting ${EnvName} enviroment...`);
 
 const server = new Server();
