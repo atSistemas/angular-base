4e52c44f3acae666bee382885357efe79bf1a429
Pablo Magaz <pmagaz@atsistemas.com>
2017-01-23 17:56:11 +0100

    Add createReducer to shared

---
+++ src/base/shared/CreateReducer.ts
@@ -0,0 +1,14 @@
+import { Action } from '@ngrx/store';
+
+export function createReducer<Cr>(actionHandler: any, initialState: Cr) {
+  return (state: Cr, action: Action): Cr => {
+
+    if (!state) { return initialState; }
+
+    if (typeof actionHandler[action.type] === 'function') {
+      return actionHandler[action.type](state, action);
+    }
+
+    return state;
+  };
+};
