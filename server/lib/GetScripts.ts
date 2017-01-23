61285b3765480a69e9ff9f74e95814e1456f26b4
Pablo Magaz <pmagaz@atsistemas.com>
2017-01-23 12:06:28 +0100

    File to handle js scripts on server

---
+++ server/lib/GetScripts.ts
@@ -0,0 +1,25 @@
+import * as path from 'path';
+
+import { ENV } from '../../src/base/';
+import * as FileSystem from '../../src/base/shared/FileSystem';
+
+export default function getScripts(file) {
+
+  /*
+  TODO ADD CACHE HASH CONTROL
+  const scriptPath = (ENV === 'development' && file === 'vendor')
+  ? path.resolve( __dirname, '../../dist/vendor-hashes.json')
+  : path.resolve( __dirname, '../../dist/output-hashes.json');
+
+  try {
+    const content = JSON.parse(FileSystem.readFile(scriptPath, 'utf8'));
+    const node = content.files[file];
+    return (node instanceof Array ) ? `<script src="/${node[0]}"></script>` : `<script src="/dlls/${node}"></script>`;
+    } catch (e) {
+      console.error(e);
+    }*/
+
+    if(ENV === 'development' && file === 'vendor') return `<script src="/dlls/${file}.js"></script>`;
+    else if(ENV === 'production' && file === 'vendor') return '';
+    else return `<script src="/${file}.js"></script>`;
+}
