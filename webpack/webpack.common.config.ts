b87033bd5443ec2f0308c95d2fc7341589bc7f77
Pablo Magaz <pmagaz@atsistemas.com>
2017-01-24 13:05:41 +0100

    Remove rxjs imports from common config

--- webpack/webpack.common.config.ts
+++ webpack/webpack.common.config.ts
@@ -22,7 +22,7 @@ export const polyfills = [
   'core-js/client/shim',
   'zone.js/dist/zone',
   'zone.js/dist/long-stack-trace-zone',
-  'ts-helpers',
+  'ts-helpers'
 ];
 
 export const entry = {
@@ -35,13 +35,6 @@ export const entry = {
       '@angular/platform-browser-dynamic',
       '@angular/router',
       'angular2-template-loader',
-      'immutable',
-      'typed-immutable-record',
-      'ng2-redux',
-      'ng2-redux-router',
-      'redux',
-      'redux-observable',
-      'rxjs',
     ],
     polyfills: polyfills
 };
