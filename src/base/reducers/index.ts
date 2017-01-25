51fb1e185d950c3abf2e0d95af69687a0db6bb42
Pablo Magaz <pmagaz@atsistemas.com>
2017-01-25 13:28:35 +0100

    Improve store configuration and combine reducers

--- src/base/reducers/index.ts
+++ src/base/reducers/index.ts
@@ -1,25 +1,15 @@
-import { compose } from '@ngrx/core/compose';
-import { storeFreeze } from 'ngrx-store-freeze';
-import { storeLogger } from 'ngrx-store-logger';
 import { ActionReducer, combineReducers } from '@ngrx/store';
 import { routerReducer, RouterState } from '@ngrx/router-store';
-
 import { MainReducer } from '../../app/containers/main/reducers';
-import { MainState } from '../models';
-import { RequestMiddleware } from '../middleware/requestMiddleware';
+import { configureStore } from '../store';
 
-export interface AppState {
-  router: RouterState;
-  main: MainState;
-}
-
-export const reducers = {
+export const rootReducer = combineReducers({
   main: MainReducer,
   router: routerReducer,
-};
+});
 
-const enhancer = compose(RequestMiddleware, storeLogger(), combineReducers)(reducers);
+const store = configureStore(rootReducer);
 
-export function rootReducer(state: any, action: any) {
-    return enhancer(state, action);
+export function RootReducer(state: any, action: any) {
+  return store(state, action);
 }
