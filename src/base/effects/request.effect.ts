2c95c914840662c09fe86418c3b0ab88ba8d7fff
Pablo Magaz <pmagaz@atsistemas.com>
2017-03-27 09:49:09 +0200

    Fix request effect

--- src/base/effects/request.effect.ts
+++ src/base/effects/request.effect.ts
@@ -11,25 +11,13 @@ export class RequestEffect {
     private actions$: Actions,
   ) { }
 
-  // FIXME
-  private getRequestActions(): string {
-    const obj = ActionTypes;
-    const arr = [];
-    for (let key in obj) {
-      if (key.includes('REQUEST')) {
-        arr.push(key);
-      }
-    }
-    return arr.toString();
-  }
-
   private getActionPrefix(action): string {
     return action.substr(0, action.indexOf('_'));
   }
 
   @Effect()
     private main$ = this.actions$
-    .ofType(this.getRequestActions())
+    .filter(action => action.payload && action.payload.request)
     .switchMap(action => action.payload.request
       .mergeMap((res: any) => Observable.of({
           type: `${this.getActionPrefix(action.type)}_SUCCESS`,
