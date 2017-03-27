b47845fe9f8a2d9a5139d3efd53783cf14785b77
Pablo Magaz <pmagaz@atsistemas.com>
2017-03-27 13:28:47 +0200

    Remove pathMatch

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { WelcomeComponent } from '../base/components/welcome';
 
 export const routes: Routes = [
-  { path: '', component: WelcomeComponent, pathMatch: 'full'},
+  { path: '', component: WelcomeComponent },
   { path: 'main', loadChildren: './containers/main/index#MainModule?sync=true' },
   { path: 'lazy', loadChildren: './containers/+lazy/index#LazyModule' },
 ];
