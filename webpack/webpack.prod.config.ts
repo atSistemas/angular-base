5b152f5f2f788c21828a2cc14af3f6902df11388
José Carlos Lama Ponce <jclama@atsistemas.com>
2017-03-29 12:54:52 +0200

    Solución error de importación

--- webpack/webpack.prod.config.ts
+++ webpack/webpack.prod.config.ts
@@ -2,14 +2,10 @@ import * as path from 'path';
 import * as common from './webpack.common.config';
 
 const webpack = require('webpack');
-const BabiliPlugin = require('babili-webpack-plugin');
 const CommonsChunkPlugin = require('webpack/lib/optimize/CommonsChunkPlugin');
 const AddAssetHtmlPlugin = require('add-asset-html-webpack-plugin');
-<<<<<<< HEAD
 const BabiliPlugin = require('babili-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-=======
->>>>>>> 29d2987e60f9ce2e7ad308bbcf9af370bf038a53
 
 export const cache = common.cache;
 export const output = common.output;
