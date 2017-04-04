f5a24d7bdc9d4c5536df0ac41a8a4ee14015a9c1
Pablo Magaz <pmagaz@atsistemas.com>
2017-04-04 09:40:53 +0200

    Lint server code

--- server/middleware/dev-middleware.ts
+++ server/middleware/dev-middleware.ts
@@ -8,32 +8,32 @@ const webpackHotMiddleware = require('webpack-hot-middleware');
 
 export default function():RequestHandler[] {
 
-    const config = require('../../webpack').default;
-    const compiler = timedCompiler(config);
+  const config = require('../../webpack').default;
+  const compiler = timedCompiler(config);
 
-    const serverOptions = {
-      hot: true,
-      lazy: false,
-      quiet: true,
-      watch: true,
-      noInfo: true,
-      progress: false,
-      stats: { colors: true },
-      publicPath: config.output.publicPath || '/',
-      headers: { 'Access-Control-Allow-Origin': '*' }
-    };
+  const serverOptions = {
+    hot: true,
+    lazy: false,
+    quiet: true,
+    watch: true,
+    noInfo: true,
+    progress: false,
+    stats: { colors: true },
+    publicPath: config.output.publicPath || '/',
+    headers: { 'Access-Control-Allow-Origin': '*' }
+  };
 
-    const devMiddleware = webpackDevMiddleware(compiler, serverOptions);
+  const devMiddleware = webpackDevMiddleware(compiler, serverOptions);
 
-    const hotMiddleware = webpackHotMiddleware(compiler, {
-      log: false, path: '/__webpack_hmr', heartbeat: 10 * 1000
-    });
+  const hotMiddleware = webpackHotMiddleware(compiler, {
+    log: false, path: '/__webpack_hmr', heartbeat: 10 * 1000
+  });
 
-    const middlewares: RequestHandler[] = [
-      devMiddleware,
-      hotMiddleware,
+  const middlewares: RequestHandler[] = [
+    devMiddleware,
+    hotMiddleware,
 
-    ];
+  ];
 
-    return middlewares;
+  return middlewares;
 };
--- server/middleware/prod-middleware.ts
+++ server/middleware/prod-middleware.ts
@@ -7,17 +7,17 @@ const webpack = require('webpack');
 
 export default function():RequestHandler[] {
 
-    const config = require('../../webpack').default;
+  const config = require('../../webpack').default;
 
-    const compiler = webpack(config);
+  const compiler = webpack(config);
 
-    compiler.plugin('done', function() {
-      base.console.success(`Bundled project in  ms!`);
-    });
+  compiler.plugin('done', function() {
+    base.console.success(`Bundled project in  ms!`);
+  });
 
-    const middlewares:RequestHandler[] = [
-      compression()
-    ];
+  const middlewares:RequestHandler[] = [
+    compression()
+  ];
 
-    return middlewares;
+  return middlewares;
 }
