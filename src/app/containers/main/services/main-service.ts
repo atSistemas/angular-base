828e15c2d491d99898f20a6202441742700c28cb
Jose Manuel Bermudo <josemanuelbd@gmail.com>
2017-04-06 17:30:43 +0200

    implementing testing

--- src/app/containers/main/services/main-service.ts
+++ src/app/containers/main/services/main-service.ts
@@ -1,7 +1,7 @@
 import { Http } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-
+import 'rxjs/Rx';
 import { BaseService } from 'base/services/BaseService';
 
 @Injectable()
