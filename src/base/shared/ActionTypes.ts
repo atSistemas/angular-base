e6a874368104301618dbbbac30a2cb323b61a63e
Pablo Magaz <pmagaz@atsistemas.com>
2017-04-19 18:20:55 +0200

    Add createActionType

---
+++ src/base/shared/ActionTypes.ts
@@ -0,0 +1,12 @@
+import { BaseError } from './BaseError';
+
+export function createActionType<K extends string>(ActionTypes: K[]): { [P in K]: string } {
+  if (ActionTypes.constructor !== Array)  throw new BaseError('You should pass an array of actions');
+
+  const actions = {};
+  ActionTypes.forEach((type: string) => {
+    actions[type] = type;
+  });
+
+  return Object.freeze(actions) as Readonly<{ [P in K]: string }>;
+}
--- src/base/shared/Errors.ts
+++
@@ -1,9 +0,0 @@
-// tslint:disable:no-invalid-this
-export  function ReactBaseError(msg) {
-  this.msg = '[BASE ERROR] ' + msg;
-  this.name = 'React Base Error';
-  this.stack = (new Error()).stack;
-}
-
-ReactBaseError.prototype = Object.create(Error.prototype);
-ReactBaseError.prototype.constructor = ReactBaseError;
