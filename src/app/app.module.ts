056180375a78ec2bc7eb0d71f41dbaa738fb2382
Fernando Ramos <ferramgon1@gmail.com>
2017-12-07 12:06:36 +0100

    sorts out container module importation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,33 @@
 import { HttpModule } from '@angular/http';
 import { ApplicationRef, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { Store, State } from 'base';
 import { BaseImports } from 'base/imports/';
 import { BaseProviders } from 'base/providers/';
 
 import { MainModule } from './containers/main/main.module';
+import { CalculatorModule } from './containers/calculator/calculator.module';
+import { WeatherModule } from './containers/weather/weather.module';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './components/topBar/topBar.component';
 
 @NgModule({
   bootstrap: [ AppComponent ],
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent
   ],
   imports: [
     BaseImports,
     BrowserModule,
     HttpModule,
-    MainModule
+    MainModule,
+    CalculatorModule,
+    WeatherModule,
+    RouterModule
   ],
   providers: [ BaseProviders ]
 })
--- src/app/containers/main/main.module.ts
+++ src/app/containers/main/main.module.ts
@@ -5,9 +5,6 @@ import { RouterModule } from '@angular/router';
 import { MainContainer } from './main.container';
 
 import { LogoComponent } from '../../components/logo/logo.component';
-import { CalculatorModule } from '../calculator/calculator.module';
-import { WeatherModule } from '../weather/weather.module';
-import { TopBarComponent } from '../../components/topBar/topBar.component';
 
 import { MainActions } from './actions';
 import { MainService } from './services';
@@ -15,22 +12,14 @@ import { MainService } from './services';
 @NgModule({
   declarations: [
     MainContainer,
-    LogoComponent,
-    TopBarComponent
+    LogoComponent
   ],
   imports: [
-    CommonModule,
-    CalculatorModule,
-    WeatherModule,
-    RouterModule
+    CommonModule
   ],
   providers: [
     MainActions,
     MainService
-  ],
-  exports: [
-    TopBarComponent,
-    LogoComponent
   ]
 })
 
