6a8b48c3ffa58a0ed843c79da27daec2df258c73
Jesús González Jaén <jesus92gz@gmail.com>
2018-01-05 14:40:27 +0100

    LazyContainer: initial testing and mock store

--- src/app/containers/+lazy/spec/lazy.container.spec.ts
+++ src/app/containers/+lazy/spec/lazy.container.spec.ts
@@ -1,8 +1,11 @@
 import { TestBed, ComponentFixture, async, getTestBed } from '@angular/core/testing';
 import { DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
+import { Store } from '@ngrx/store';
 
 import { expect } from 'chai';
+
+import { MockStore } from '../../../../base/store/mock-store';
 
 import { LazyContainer } from '../lazy.container';
 import { LazyActions } from '../actions';
@@ -13,28 +16,38 @@ describe('container: lazy', () => {
   let de: DebugElement;
   let el: HTMLElement;
 
+  let _store: Store<any>;
+  let _lazyActions: LazyActions;
+
   beforeEach(() => {
+    TestBed.resetTestingModule();
     TestBed.configureTestingModule({
-      declarations: [ LazyContainer ]
+      declarations: [ LazyContainer ],
+      providers: [
+        LazyActions,
+        {
+          provide: Store,
+          useValue: new MockStore({lazy: {message: 'Lazy Container'}})
+        }
+       ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(LazyContainer);
+
+    _store = fixture.debugElement.injector.get(Store);
+    _lazyActions = fixture.debugElement.injector.get(LazyActions);
+
     component = fixture.componentInstance;
 
-    de = fixture.debugElement.query(By.css('p'));
-    el = de.nativeElement;
-
-  });
-
-  afterEach(() => {
-    getTestBed().resetTestingModule();
+    de = fixture.debugElement;
+    el = fixture.nativeElement;
   });
 
   describe('layout', () => {
-    it('no messages in the DOM until component is rendered', () => {
+    it('should render "Lazy Container"', () => {
       fixture.detectChanges();
-      expect(el.textContent).to.equal('');
+      expect(el.querySelector('p').textContent).to.equal('Lazy Container');
     });
   });
-  
+
 });
