6259a48c4277943915c4f563d5aaf4fbefa9af82
Fernando Ramos <ferramgon1@gmail.com>
2018-01-12 14:05:27 +0100

    sets weather API as an individual export

--- src/app/containers/weather/config/index.ts
+++ src/app/containers/weather/config/index.ts
@@ -1,5 +1,2 @@
-export const config = {
-  GOOGLE_MAPS_API_KEY: 'AIzaSyAUrK9ZaUÑ0Ga-RZYYFukBuTNm0qO3GbNI',
-  OPEN_WEATHER_MAP_API_URL: 'http://api.openweathermap.org/data/2.5',
-  OPEN_WEATHER_MAP_API_KEY: '3cc68ed2bf53673eaf25aae12fb16469'
-};
+export * from './google-maps.config';
+export * from './open-weather-map.config';
